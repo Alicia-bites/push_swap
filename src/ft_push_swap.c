aee584c3b3dbae6229a8224204f9628ff9ff7d6f
Alicia MARCHAND <amarchan@e1r3p2.42.fr>
2022-03-09 16:28:03 +0100

    infinite loop issue and usage display

--- src/ft_push_swap.c
+++ src/ft_push_swap.c
@@ -5,14 +5,14 @@ int	main(int argc, char **argv)
 	t_stack	stack;
 	int	i;
 
-	i = 0;
-	if (argc != 1)
+	i = 1;
+	if (argc <= 1)
 		return (ft_panic(1));
 	stack.size = ft_strlen(argv[1]);
 	if (stack.size == 0)
 		return (ft_panic(EMPTY_STR));
 	while (argv[i])
-		ft_parse(argv[i]);
+		ft_parse(argv[i++]);
 	// ft_create_list(stack);
 	return (0);
 }
