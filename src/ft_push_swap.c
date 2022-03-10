a1a04c328cf59bebe928b950be849db213895f69
Alicia MARCHAND <amarchan@e1r10p11.42.fr>
2022-03-10 15:25:39 +0100

    change stack.size for size

--- src/ft_push_swap.c
+++ src/ft_push_swap.c
@@ -1,18 +1,27 @@
 #include "../headers/ft_push_swap.h"
 
+
 int	main(int argc, char **argv)
 {
-	t_stack	stack;
+	t_stack *stack;
+	t_stack *iterator;
 	int	i;
+	int size;
 
 	i = 1;
 	if (argc <= 1)
 		return (ft_panic(1));
-	stack.size = ft_strlen(argv[1]);
-	if (stack.size == 0)
+	size = ft_strlen(argv[1]);
+	if (size == 0)
 		return (ft_panic(EMPTY_STR));
 	while (argv[i])
-		ft_parse(argv[i++]);
-	// ft_create_list(stack);
+		stack = ft_parse(argv[i++]);
+	printf("stack : %p\n", stack);
+	iterator = stack;
+	while (iterator)
+	{
+		printf("iterator->num = %d\n", iterator->num);
+		iterator = iterator->next;
+	}
 	return (0);
 }
