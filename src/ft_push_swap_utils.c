446e7357da556767ff11f645b4a777382c34b89d
Alicia MARCHAND <amarchan@e1r10p11.42.fr>
2022-03-10 15:24:56 +0100

    moved ft_create_lst to ft_parse

--- src/ft_push_swap_utils.c
+++ src/ft_push_swap_utils.c
@@ -6,22 +6,9 @@
 /*   By: amarchan <amarchan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/07 14:53:08 by amarchan          #+#    #+#             */
-/*   Updated: 2022/03/09 16:00:06 by amarchan         ###   ########.fr       */
+/*   Updated: 2022/03/10 12:10:16 by amarchan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../headers/ft_push_swap.h"
 
-ft_create_list(char **pile, int size)
-{
-	int	i;
-	int	j;
-	i = 0;
-	while(i < size)
-	{	
-		while (argv[i][j])
-		{
-			
-		}
-	}
-}
