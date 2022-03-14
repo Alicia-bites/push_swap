b5a3cc4b52c4f9363fa275444c83d707b10ca736
alicia <alicia.mrchd@gmail.com>
2022-03-14 14:50:59 +0100

    ft_swap function done + if less than 2 arguments, error

--- src/ft_push_swap.c
+++ src/ft_push_swap.c
@@ -9,18 +9,27 @@ int	main(int argc, char **argv)
 	int size;
 
 	i = 1;
-	if (argc <= 1)
+	if (argc <= 2)
 		return (ft_panic(1));
 	size = ft_strlen(argv[1]);
 	if (size == 0)
 		return (ft_panic(EMPTY_STR));
 	while (argv[i])
 		stack = ft_parse(argv[i++]);
-	printf("stack : %p\n", stack);
+	//printf("stack : %p\n", stack);
 	iterator = stack;
 	while (iterator)
 	{
 		printf("iterator->num = %d\n", iterator->num);
+		iterator = iterator->next;
+	}
+	ft_swap(stack);
+	puts("----------------------------------------");
+	iterator = stack;
+	while (iterator)
+	{
+		printf("iterator->num = %d\n", iterator->num);
+		// printf("iterator->next->num = %d\n", iterator->next->num);
 		iterator = iterator->next;
 	}
 	return (0);
