bf5e8f1cd444d22aa7b2c7eaaa4813037a839dfe
alicia <alicia.mrchd@gmail.com>
2022-03-14 14:50:33 +0100

    ft_swap function done

--- moves/swaps.c
+++ moves/swaps.c
@@ -1,4 +1,26 @@
-void	ft_sa()
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   swaps.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amarchan <amarchan@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/14 14:45:26 by amarchan          #+#    #+#             */
+/*   Updated: 2022/03/14 14:47:04 by amarchan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../headers/ft_push_swap.h"
+
+//swap the first 2 elts of the stack
+void	ft_swap(t_stack *stack)
 {
-	
+	int tmp;
+
+	if (stack)
+	{
+		tmp = stack->num;
+		stack->num = stack->next->num;
+		stack->next->num = tmp;
+	}
 }
