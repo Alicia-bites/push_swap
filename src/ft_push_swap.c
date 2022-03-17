eac195f9629e2d669a6cfd2567cf842e5909a5df
alicia <alicia.mrchd@gmail.com>
2022-03-17 16:56:14 +0100

    can sort list of 3 max

--- src/ft_push_swap.c
+++ src/ft_push_swap.c
@@ -1,4 +1,42 @@
 #include "../headers/ft_push_swap.h"
+//check if list is sorted
+//if elt is bigger than next elt, return 0. 
+int	is_sorted(t_stack *stack)
+{	
+	t_stack *iterator;
+
+	iterator = stack;
+	while (iterator->next)
+	{
+		if (iterator->num > iterator->next->num)
+			return (0);
+		iterator = iterator->next;
+	}
+	return (1);
+}
+
+//sort a list of max 3 elements
+void	sort_3(t_stack *stack)
+{
+	t_stack *last_elt;
+
+	last_elt = stack;
+	while (last_elt->next)
+		last_elt = last_elt->next;
+	while (!is_sorted(stack))
+	{
+		if (stack->num > last_elt->num)
+		{
+			if (stack->num > stack->next->num)
+				ft_ra(stack);
+			else
+				ft_rra(stack);
+		}
+		else
+			ft_sa(stack);
+	}
+}
+
 void	ft_print_stack(t_stack *stack)
 {
 	t_stack *iterator;
@@ -31,7 +69,8 @@ int	main(int argc, char **argv)
 	//ft_ra(a);
 	//ft_rb(b);
 	//ft_rr(a, b);
-	ft_rra(a);
+	//ft_rra(a);
+	sort_3(a);
 	puts("------------------");
 	ft_print_stack(a);
 	//ft_print_stack(b);
